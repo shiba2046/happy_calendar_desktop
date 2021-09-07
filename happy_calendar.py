c47a74d30c0268cad36e7d8adc89a0db33606304
shiba2046 <17551496+shiba2046@users.noreply.github.com>
2021-09-07 09:27:42 +0800

    Modifed to work under Win10

--- happy_calendar.py
+++ happy_calendar.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#%%
 """ 
 happy_calendar.py
 Download happy calendar
@@ -18,37 +19,40 @@ calendar_jpg = wsl.USERPROFILE / 'Pictures/desktop-background/right/happiness_ca
 def getURL(href):
   return requests.compat.urljoin('https://www.actionforhappiness.org', href)
 
-def getCalendar():
+#%%
+# def getCalendar():
   # Get the directorypage
-  r = requests.get(getURL('/calendars')) 
+r = requests.get(getURL('/calendars')) 
+
+
+#%%
+if r.status_code == 200:
+  soup = BeautifulSoup(r.text, 'html.parser')
   
+  # Find the first img tag with width=400
+  # Get parents, and href
+  month_url = soup.find('img', attrs={'width':'400'}).parent.attrs['href']
+  print(f'Month URL: {month_url}')
+  
+  r = requests.get(getURL(month_url))
   if r.status_code == 200:
     soup = BeautifulSoup(r.text, 'html.parser')
     
-    # Find the first img tag with width=400
-    # Get parents, and href
-    month_url = soup.find('img', attrs={'width':'400'}).parent.attrs['href']
-    print(f'Month URL: {month_url}')
+    # Find the URL to the actual calendar JPG
+    image_url = soup.find(lambda el: el.name == 'a' and 'JPG' in el.text).attrs['href']
+    print(f'Image URL: {image_url}')
+    r = requests.get(getURL(image_url), stream=True)
     
-    r = requests.get(getURL(month_url))
     if r.status_code == 200:
-      soup = BeautifulSoup(r.text, 'html.parser')
-      
-      # Find the URL to the actual calendar JPG
-      image_url = soup.find(lambda el: el.name == 'a' and 'JPG' in el.text).attrs['href']
-      print(f'Image URL: {image_url}')
-      r = requests.get(getURL(image_url), stream=True)
-      
-      if r.status_code == 200:
-        with open(calendar_jpg, 'wb') as f:
-          for chunk in r.iter_content(1024):
-            f.write(chunk)
-        print(f'Written to {calendar_jpg}')
+      with open(calendar_jpg, 'wb') as f:
+        for chunk in r.iter_content(1024):
+          f.write(chunk)
+      print(f'Written to {calendar_jpg}')
 
 
-if __name__ == '__main__':
-  getCalendar()
-  import bgChanger3
+# if __name__ == '__main__':
+#   getCalendar()
+import bgChanger3
 
   # from winChangeBackground import setWallpaper
   
